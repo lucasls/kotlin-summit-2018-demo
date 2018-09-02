a33fb60a20ab2d41abfe504ed67e3dba376fba3b
Lucas Laurindo dos Santos <lucas.santos@ifood.com.br>
2018-09-01 22:24:25 -0300

    Creating Gateway and Antifraud mock services

---
+++ gateway-antifraud-mock/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const delay = require('delay')
+const uuidv4 = require('uuid/v4');
+
+const app = express()
+const port = 3000
+
+function randomInt(from, to) {
+    return from + Math.floor(Math.random() * (to - from))
+}
+
+app.use(express.json())
+
+app.post('/antifraud/validate', async (request, response) => {
+    await delay(randomInt(20, 300))
+
+    const r = Math.random()
+    let status
+
+    if (r < 0.95) {
+        status = "APPROVED"
+    } else if (r < 0.98) {
+        status = "REPROVED"
+    } else {
+        status = "FAILED"
+    }
+    
+    response.send({
+        "status": status,
+        "reference": request.body.reference
+    })
+})
+
+app.post('/gateway/authorize', async (request, response) => {
+    await delay(randomInt(100, 1000))
+
+    const r = Math.random()
+    let status
+
+    if (r < 0.85) {
+        status = "AUTHORIZED"
+    } else if (r < 0.95) {
+        status = "DECLINED"
+    } else {
+        status = "FAILED"
+    }
+
+    let tid
+    if (status == "AUTHORIZED") {
+        tid = uuidv4()
+    } else {
+        tid = null
+    }
+
+    response.send({
+        "status": status,
+        "tid": tid,
+        "reference": request.body.reference
+    })
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
